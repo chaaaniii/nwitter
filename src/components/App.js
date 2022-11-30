91212f1a7d7a609c2c21d4a7a104eea4fa303232
chaaaniii <104333938+chan123456789@users.noreply.github.com>
2022-11-30 21:48:14 +0900

    SignIn

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import AppRouter from "./Router";
+import AppRouter from "components/Router";
+import {authService} from "fbase";
 
 function App() {
-  const [isLoggedIn, setisLoggedIn] = useState(true);
+  const [isLoggedIn, setisLoggedIn] = useState(authService.currentUser);
   return(
     <>
-    <AppRouter isLoggedIn={isLoggedIn}/>;
+    <AppRouter isLoggedIn={isLoggedIn}/>
     <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   ) 
