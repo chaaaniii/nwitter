6e3ad61cc13307790b524de9b7bb4dec726fa6d2
chaaaniii <104333938+chan123456789@users.noreply.github.com>
2022-12-18 14:23:11 +0900

    profile update

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,35 +1,36 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
 
-const AppRouter = ({ isLoggedIn , userObj }) => { //app에서 userObj를 받고
+const AppRouter = ({ isLoggedIn, userObj }) => {
+  //app에서 userObj를 받고
 
-    return (
-        <Router>
-            {isLoggedIn && <Navigation />}
-            <Switch>
-                {isLoggedIn ? (
-                    <>
-                        <Route exact path="/">
-                            {/* Home에 userObj의 prop을 전달 */}
-                            <Home userObj={userObj}/> 
-                        </Route>
-                        <Route exact path="/profile">
-                            <Profile />
-                        </Route>
-                    </>
-                ) : (
-                    <>
-                        <Route exact path="/">
-                            <Auth />
-                        </Route>
-                    </>
-                )}
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              {/* Home에 userObj의 prop을 전달 */}
+              <Home userObj={userObj} />
+            </Route>
+            <Route exact path="/profile">
+              <Profile userObj={userObj} />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 };
 export default AppRouter;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,28 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
-    const history = useHistory();
-    const onLogOutClick = () => {
-        authService.signOut();
-        history.push('/');
-    }
-    return(
-        <>
-        <button onClick={onLogOutClick}>Log Out</button>
-        </>
-    )
-}
+export default ({ userObj }) => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createAt")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
